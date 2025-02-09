8764480ed29957e4b81fc450d6ca50d7b2544bd0
kannan11 <kannan11@node-0.mymachine.rise-pg0.utah.cloudlab.us>
2025-02-09 06:26:43 -0700

    package installation

---
+++ homework1/install_packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+sudo apt update; 
+
+sudo sed -i 's/# deb-src/deb-src/' /etc/apt/sources.list
+
+sudo apt build-dep linux linux-image-unsigned-$(uname -r)
+sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+sudo apt install git
+
+chmod a+x debian/rules
+chmod a+x debian/scripts/*
+chmod a+x debian/scripts/misc/*
+fakeroot debian/rules clean
+#fakeroot debian/rules editconfigs # you need to go through each (Y, Exit, Y, Exit..) or get a complaint about config later
+fakeroot debian/rules clean
+scripts/config --disable SYSTEM_TRUSTED_KEYS
+scripts/config --disable SYSTEM_REVOCATION_KEYS
