2222564add830dec6ad51fc7a214a4c5949724f1
longtran1904 <tranhoanglong1904@gmail.com>
2025-05-04 20:03:47 -0600

    revert

--- project1/compile_os_quick.sh
+++ project1/compile_os_quick.sh
@@ -1,25 +1,25 @@
-#!/bin/bash -x
-#sudo apt update; sudo apt-get install -y libdpkg-dev kernel-package libncurses-dev
+!/bin/bash -x
+sudo apt update; sudo apt-get install -y libdpkg-dev kernel-package libncurses-dev
 
-# PROC=`nproc`
-# export CONCURRENCY_LEVEL=$PROC
-# export CONCURRENCYLEVEL=$PROC
+PROC=`nproc`
+export CONCURRENCY_LEVEL=$PROC
+export CONCURRENCYLEVEL=$PROC
 
-# sudo cp /boot/config-$(uname -r) .config
-# #make menuconfig
+sudo cp /boot/config-$(uname -r) .config
+#make menuconfig
 
-# scripts/config --disable SYSTEM_TRUSTED_KEYS
-# scripts/config --disable SYSTEM_REVOCATION_KEYS
+scripts/config --disable SYSTEM_TRUSTED_KEYS
+scripts/config --disable SYSTEM_REVOCATION_KEYS
 
-# make oldconfig
+make oldconfig
 
-# touch REPORTING-BUGS
-# #sudo make clean -j
-# #sudo make prepare
-# # sudo make -j$PROC
-# sudo make modules -j$PROC
-# sudo make modules_install
-# sudo make install
+touch REPORTING-BUGS
+#sudo make clean -j
+#sudo make prepare
+# sudo make -j$PROC
+sudo make modules -j$PROC
+sudo make modules_install
+sudo make install
 
 y="5.15.178"
 
